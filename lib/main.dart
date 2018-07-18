0d70a03f3a36591b8780eecb5ac06b3d0789179c
vikbert <segentor@gmail.com>
2018-07-18 15:53:08 +0200

    add new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './product_manager.dart';
+import 'pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,12 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.orange,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ListDemo'),
-        ),
-        body: ProductManager(),
-      ),
+      home: HomePage(),
     );
   }
 }
